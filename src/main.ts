652b38c53d917fdd8fc99060c65132c74bebbc0a
BezaTheCat <beamtamamoon@gmail.com>
2025-03-03 14:40:58 +0700

    Add imageUploadInterceptor & getBaseUrl

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { join } from 'path';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
       whitelist: true,
     }),
   );
+
+  app.useStaticAssets(join(__dirname, '..', 'assets'), {
+    prefix: '/assets', // Serve static files from /uploads
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
---
+++ src/utils.ts
@@ -0,0 +1,30 @@
+import { BadRequestException } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { randomBytes } from 'crypto';
+import { Request } from 'express';
+import { diskStorage } from 'multer';
+import { extname } from 'path';
+
+export function getBaseUrl(req: Request): string {
+  const protocol = req.headers['x-forwarded-proto'] || req.protocol;
+  return process.env.BASE_URL || `${protocol}://${req.get('host')}`;
+}
+
+export const imageUploadInterceptor = (fieldName: string) =>
+  FileInterceptor(fieldName, {
+    fileFilter: (req, file, callback) => {
+      const allowedMimeTypes = ['image/jpeg', 'image/png', 'image/gif', 'image/webp'];
+      if (!allowedMimeTypes.includes(file.mimetype)) {
+        return callback(new BadRequestException('Only image files (JPG, PNG, GIF, WEBP) are allowed! >:<'), false);
+      }
+      callback(null, true);
+    },
+    storage: diskStorage({
+      destination: './assets/uploads/image',
+      filename: (req, file, callback) => {
+        const randomId = randomBytes(8).toString('hex');
+        const ext = extname(file.originalname);
+        callback(null, `${randomId}_${file.originalname}`);
+      },
+    }),
+  });
